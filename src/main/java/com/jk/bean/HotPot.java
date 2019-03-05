41bb65186a8f0019f6a21cc0dc299aef809e64d7
HaoHuiLin <1091148413@qq.com>
2019-03-05 16:24:45 +0800

    热点资讯bean提交

---
+++ src/main/java/com/jk/bean/HotPot.java
@@ -0,0 +1,27 @@
+package com.jk.bean;
+
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * 热点资讯
+ */
+@Data
+public class HotPot {
+
+    private Integer id;
+
+    private String title; //标题
+
+    private String total; //图片
+
+    private String video; //视频
+
+    private Integer seeCount; //观看人数
+
+    private Date time; //时间
+
+    private String content; //文章
+
+}
