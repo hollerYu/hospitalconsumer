39ad6533b03a910a0789242154523dffc4917ae4
liuxingbo521 <liuxingbo9298@dingtalk.com>
2019-03-06 09:57:10 +0800

    刘兴博

---
+++ src/main/java/com/jk/service/impl/LoginServiceImpl.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (C), 2015-2019, XXX有限公司
+ * FileName: LoginServiceImpl
+ * Author:   SWORD
+ * Date:     2019/3/6 9:50
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.jk.service.impl;
+
+import com.jk.bean.User;
+import com.jk.mapper.LoginMapper;
+import com.jk.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author SWORD
+ * @create 2019/3/6
+ * @since 1.0.0
+ */
+@Service
+public class LoginServiceImpl implements LoginService {
+
+    @Autowired
+    private LoginMapper loginMapper;
+
+    @Override
+    public User LoginUserByYhMchByYhMm(User user) {
+        return loginMapper.LoginUserByYhMchByYhMm(user);
+    }
+}
